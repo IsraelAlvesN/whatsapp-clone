501ca3f6fe0e00cb797de7bf1df7ab333053b8bf
Israel Alves <devisrael98@gmail.com>
2023-04-10 19:04:14 -0300

    Separando a tela em sessões e aplicando css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import * as Icon from '@mui/icons-material';
+
 import './App.css'
 
 export default () => {
   return(
-    <div>Começando</div>
+    <div className='app-window'>
+      <div className='sidebar'> 
+        <header>
+          <img className='header--avatar' src="https://www.nicepng.com/png/detail/207-2074901_woman-icon-avatar-icon.png" alt="" />
+          <div className='header--buttons'>
+            <div className='header--btn'>
+              <Icon.DonutLarge style={{color: '#919191'}}/>
+            </div>
+            <div className='header--btn'>
+              <Icon.Chat style={{color: '#919191'}}/>
+            </div>
+            <div className='header--btn'>
+              <Icon.MoreVert style={{color: '#919191'}}/>
+            </div>
+          </div>
+        </header>
+        <div className='search'>
+          ...
+        </div>
+        <div className='chatlist'>
+          ...
+        </div>
+      </div>
+      <div className='contentarea'>
+        ...
+      </div>
+    </div>
   )
 }
